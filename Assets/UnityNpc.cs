1c8540a2d435de3e92fb6c5c2a4998d9742e1bb3
Steven Schenk <stevenschenk74@hotmail.com>
2016-11-21 13:48:00 +0100

    Added stereoscopic for 3D effects

--- Assets/UnityNpc.cs
+++ Assets/UnityNpc.cs
@@ -120,4 +120,4 @@ public class UnityNpc : MonoBehaviour
             currentEvent.AssociatedActions.ToDictionary(x => x, x => ActionsParser.EventActions[x]);
     }
 }
-                                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                                                 
