5b3ba9d10c585d1852ef7542086548af1cf3b2da
Steven Schenk <stevenschenk74@hotmail.com>
2016-11-23 13:02:13 +0100

    fix mine avatars + player collision with avatars

--- Assets/NpcObject.cs
+++ Assets/NpcObject.cs
@@ -178,4 +178,4 @@ public class NpcObject : MonoBehaviour
         Animator.SetBool(animationName, false);
     }
 }
-                            
+                                                                                     
